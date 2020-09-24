32f22183e7659bd128290d81134c3b15ca9cc155
sanstwy777 <sanstwy777@outlook.com>
2020-09-24 23:08:16 +0800

    initial

---
+++ LiveServer/src/main/java/com/sanstwy27/demo/LiveStreamServerApplication.java
@@ -0,0 +1,13 @@
+package com.sanstwy27.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStreamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveStreamServerApplication.class, args);
+    }
+
+}
