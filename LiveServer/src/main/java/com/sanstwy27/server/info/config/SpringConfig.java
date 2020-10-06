1687326d963425064530d8e990830ec97abf0f4c
sanstwy777 <sanstwy777@outlook.com>
2020-10-06 10:55:03 +0800

    add CORS configuration

---
+++ LiveServer/src/main/java/com/sanstwy27/server/info/config/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.sanstwy27.server.info.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Sanstwy27
+ * @create 10/6/2020
+ */
+
+@Configuration
+@Profile("dev")
+public class SpringConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
+    }
+}
